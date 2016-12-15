ee7df5eca7c90a2640dbba5632c801150255d2fc
Данила Лужин <d.m.luzhin@gmail.com>
2016-12-15 16:51:52 +0300

    added sections, fixed header, animations

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+
+	/*Hamburger animation toggle*/
 	$('.hamburger').click(function(){
 		$(this).toggleClass('is-active');
 	});
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 60) {
+			$('.navbar').addClass('fixed-navbar');
+		} else {
+			$('.navbar').removeClass('fixed-navbar').show('fast');
+		}
+	});
+
 });
