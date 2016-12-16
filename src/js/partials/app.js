5adf8486ab4be94340757b0d94b8329810aeef2b
Данила Лужин <d.m.luzhin@gmail.com>
2016-12-16 16:05:01 +0300

    animated anchors

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -5,12 +5,23 @@ $(document).ready(function() {
 		$(this).toggleClass('is-active');
 	});
 
+	/*fixed header*/
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 60) {
 			$('.navbar').addClass('fixed-navbar');
 		} else {
 			$('.navbar').removeClass('fixed-navbar').show('fast');
-		}
-	});
+		};
 
+		$('.navigation a').click(function(){
+
+			$('.active').removeClass('active')
+			$(this).closest('a').addClass("active");
+
+			$('html, body').stop().animate({
+				scrollTop: $( $(this).attr('href') ).offset().top - 160 }, 250);
+
+			return false
+		});
+	});
 });
